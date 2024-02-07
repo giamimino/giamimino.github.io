c3c8d1ad92c2c5b19732fde24e167bf6b9b47ad2
ripmimino <miminoshvili2102@gmail.com>
2024-02-07 13:51:10 +0400

    test-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+      
+      reader.onload = function(e) {
+        const preview = document.getElementById('preview');
+        const img = document.createElement('img');
+        img.src = e.target.result;
+        preview.innerHTML = '';
+        preview.appendChild(img);
+      };
+      
+      reader.readAsDataURL(file);
+    }
+  });
+  
